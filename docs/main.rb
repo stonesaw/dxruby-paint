e1d5f56e6b5ec07b9e76210c140c91f576f2447e
stonesaw <ishichan1260@gmail.com>
2022-02-05 19:15:58 +0900

    fix: comment out pallete

--- docs/main.rb
+++ docs/main.rb
@@ -11,15 +11,18 @@ Window.load_resources do
   canvas = Image.new(Window.width, Window.height, C_WHITE)
 
   pen = Pen.new(:circle, 20, C_BLUE)
-  pallete = Pallete.new
+  # TODO : Error: cant use RenderTarget
+  # pallete = Pallete.new
 
   Window.loop do
     pen.update
-    pallete.update(pen)
+    # TODO
+    # pallete.update(pen)
 
     pen.draw(canvas)
     Window.draw(0, 0, canvas)
-    pallete.draw(pen)
+    # TODO
+    # pallete.draw(pen)
 
     Window.draw_font(0, 0, "fps: #{Window.real_fps}", Font.default, color: [30, 30, 30])
   end
